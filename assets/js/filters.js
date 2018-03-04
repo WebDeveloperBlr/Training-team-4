c2dd92935c99dae7d7db03bfe5677fcd69b1f309
drobch <drobyshks@gmail.com>
2018-03-04 15:52:35 +0200

    vacancies:

--- assets/js/filters.js
+++ assets/js/filters.js
@@ -2,7 +2,7 @@
 $(document).ready(function () {
 
   let rows = $(".vacancies-block__table__row");
-  let cols = $(".vacancies-block__table-col")
+  let cols = $(".vacancies-block__table-col");
   let experiences = ["any", "junior", "middle", "senior"];
 
   let filter = {
@@ -52,77 +52,78 @@ $(document).ready(function () {
       return false;
   }
 
-  $("#vacancies-block__filters-bar__sort-button").on('click', toFilter());
+  $("#vacancies-block__filters-bar__sort-button").bind('click', toFilter());
+
+
+
+  /*** рабочая функция, c $().on() не работает ***/
+  function sortTable(n) {
+    let table, rows, switching, i, x, y, shouldSwitch, switchcount = 0;
+    let textCurItem, textNextItem;
+    let dir = "asc";
+    switching = true;
+
+    while (switching) {
+      switching = false;
+      rows = $('.vacancies-block__table__row');
+
+      for (i = 0; i < (rows.length - 1); i++) {
+
+        shouldSwitch = false;// Start by saying there should be no switching:
+        /* Get the two elements you want to compare,
+        one from current row and one from the next: */
+
+        x = ($(".vacancies-block__table-col", rows[i]))[n];
+        y = ($(".vacancies-block__table-col", rows[i + 1]))[n];
+
+        textCurItem = x.innerText.toLowerCase();
+        textNextItem = y.innerText.toLowerCase();
+
+        if (n == 2) {
+          textCurItem = parseInt(textCurItem.slice(1));
+          textNextItem = parseInt(textNextItem.slice(1));
+        }
+
+        /* Check if the two rows should switch place,
+        based on the direction, asc or desc: */
+
+        if (dir == "asc") {
+          if (textCurItem > textNextItem) {
+            // If so, mark as a switch and break the loop:
+            shouldSwitch = true;
+            break;
+          }
+        } else if (dir == "desc") {
+          if (textCurItem < textNextItem) {
+            shouldSwitch = true;// If so, mark as a switch and break the loop:
+            break;
+          }
+        }
+      }
+      if (shouldSwitch) {
+        /* If a switch has been marked, make the switch
+        and mark that a switch has been done: */
+        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+        switching = true;
+        // Each time a switch is done, increase this count by 1:
+        switchcount++;
+      } else {
+        /* If no switching has been done AND the direction is "asc",
+        set the direction to "desc" and run the while loop again. */
+
+        if (switchcount == 0 && dir === "asc") {
+          dir = "desc";
+          switching = true;
+        }
+      }
+    }
+  }
+
+  $('#table--header-col--pos').bind('click', sortTable(0));
+  $('#table--header-col--exp').bind('click', sortTable(1));
+  $('#table--header-col--sal').bind('click', sortTable(2))
 
 });
 
 
 
-
-
-$('#table--header-col--pos').on('click', sortTable(0));
-$('#table--header-col--exp').on('click', sortTable(1));
-$('#table--header-col--sal').on('click', sortTable(2))
-
-/*** рабочая функция, c $().on() не работает ***/
-function sortTable(n) {
-  let table, rows, switching, i, x, y, shouldSwitch, switchcount = 0;
-  let textCurItem, textNextItem;
-  let dir = "asc";
-  switching = true;
-
-  while (switching) {
-    switching = false;
-    rows = $('.vacancies-block__table__row');
-
-    for (i = 0; i < (rows.length - 1); i++) {
-
-      shouldSwitch = false;// Start by saying there should be no switching:
-      /* Get the two elements you want to compare,
-      one from current row and one from the next: */
-
-      x = ($(".vacancies-block__table-col", rows[i]))[n];
-      y = ($(".vacancies-block__table-col", rows[i + 1]))[n];
-
-      textCurItem = x.innerText.toLowerCase();
-      textNextItem = y.innerText.toLowerCase();
-
-      if (n == 2) {
-        textCurItem = parseInt(textCurItem.slice(1));
-        textNextItem = parseInt(textNextItem.slice(1));
-      }
-
-      /* Check if the two rows should switch place,
-      based on the direction, asc or desc: */
-
-      if (dir == "asc") {
-        if (textCurItem > textNextItem) {
-          // If so, mark as a switch and break the loop:
-          shouldSwitch = true;
-          break;
-        }
-      } else if (dir == "desc") {
-        if (textCurItem < textNextItem) {
-          shouldSwitch = true;// If so, mark as a switch and break the loop:
-          break;
-        }
-      }
-    }
-    if (shouldSwitch) {
-      /* If a switch has been marked, make the switch
-      and mark that a switch has been done: */
-      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-      switching = true;
-      // Each time a switch is done, increase this count by 1:
-      switchcount++;
-    } else {
-      /* If no switching has been done AND the direction is "asc",
-      set the direction to "desc" and run the while loop again. */
-
-      if (switchcount == 0 && dir === "asc") {
-        dir = "desc";
-        switching = true;
-      }
-    }
-  }
-}
