62de6c05bd5e389068e2faeeb35bd2b67db184c0
Panteleev <panteleevbbbogdan@gmail.com>
2018-03-19 12:49:12 +0300

    page with calendar end events on it

--- assets/js/main.js
+++ assets/js/main.js
@@ -777,7 +777,7 @@ $(document).ready(function () {
 
 
 $( "#cands-editing-icon" ).click(function() {
-    var allInputs=document.getElementsByTagName('input');
+    var allInputs=document.getElementsByClassName('inp');
     for( var i=0; i<allInputs.length; i++ ){
         $(allInputs[i]).removeClass('display-none');
         $(allInputs[i]).attr('placeholder' , $(allInputs[i]).prev().text());
